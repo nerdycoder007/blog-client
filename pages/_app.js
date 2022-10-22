6d56a6a69a8a4d3c2c2d175ab8914fbc7518fedf
Bikrant Jung Budhathoki <bikrantjung1214@gmail.com>
2022-10-22 14:21:56 +0545

    Initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+import { setContext } from "apollo-link-context";
+import { createUploadLink } from "apollo-upload-client";
+const authLink = setContext((req, prev) => {
+  return {
+    headers: {
+      Authorization: `Bearer ABC`,
+    },
+  };
+});
+const httpLink = createUploadLink(
+  { uri: "http://localhost:8000/graphql", credentials: "include" },
+  { "Apollo-Require-Preflight": "true" }
+);
+const client = new ApolloClient({
+  // link:authLink.concat(httpLink),
+  link: httpLink, // Must be link:
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {},
+      fontFamily: {},
+    },
+
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
